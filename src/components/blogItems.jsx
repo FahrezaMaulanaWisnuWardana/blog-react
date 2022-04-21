37aba1445007859ab5d10bcdb642aa9a9987f482
FahrezaMaulanaWisnuWardana <reza11.6a@gmail.com>
2022-04-21 16:36:22 +0700

    get posts data

--- src/components/blogItems.jsx
+++ src/components/blogItems.jsx
@@ -1,19 +1,30 @@
+import { Link } from "react-router-dom"
+
 const blogItem = (props) => {
-    
+
+    // Date Setting
+    let date = new Date(props.date)
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let dateFormat =`${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     return(
-        <article className="mb-3">
-            <header>
-                <h3
-                className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white"
-                style={{
-                    fontFamily:"Montserrat, sans-serif",
-                }}>
-                   {props.title}
-                </h3>
-                <small className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">Topics • July 7, 2021</small>
-            </header>
-            <p className="mt-5 text-black dark:text-slate-50">Found 99 vulnerabilities (84 moderately irrelevant, 15 highly irrelevant)</p>
-        </article>
+        <Link to={`/blog/${props.slug}`}>
+            <article className="mb-5">
+                <header>
+                    <h3
+                    className="text-3xl font-bold tracking-tight text-black dark:text-white"
+                    style={{
+                        fontFamily:"Montserrat, sans-serif",
+                    }}>
+                    {props.title}
+                    </h3>
+                    <small className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">{dateFormat}</small>
+                </header>
+                <p className="mt-2 text-black dark:text-slate-50">{props.summary}</p>
+                <small className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
+                    {props.topic}
+                </small>
+            </article>
+        </Link>
     )
 }
 export default blogItem
--- src/pages/Home/main.jsx
+++ src/pages/Home/main.jsx
@@ -1,15 +1,37 @@
+import {
+    useQuery,
+    useQueryClient
+} from 'react-query'
 import BlogItem from '../../components/blogItems'
+import axios from 'axios'
 // Package
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+    const Blogs = async () => {
+        const res = await axios.get(`http://127.0.0.1:8080/api/v1/posts`)
+        return res.data
+    }
+    const {data,status} = useQuery('posts',Blogs) 
+    if(status==='loading'){
+      return <div>Loading...</div>
+    }
     return (
         <div className='px-4 mt-10'>
-            <BlogItem title="Title"/>
-            <BlogItem title="Title"/>
-            <BlogItem title="Title"/>
-            <BlogItem title="Title"/>
-            <BlogItem title="Title"/>
+            {
+                data.data.map((post)=>(
+                    <BlogItem 
+                        key={post.id}
+                        slug={post.slug}
+                        title={post.title}
+                        summary={post.summary}
+                        topic={post.topics.map((topic)=>(
+                            `${topic.name} `
+                        ))}
+                        date={post.created_at}
+                    />
+                ))
+            }
         </div>
     )
 }
